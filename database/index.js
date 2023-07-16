9fde7ca0b31a6d4c37585ee6e64905b43d827fd3
Franco <francoherrera_8@hotmail.com.ar>
2023-07-16 00:59:10 -0300

    delete port 8004

--- database/index.js
+++ database/index.js
@@ -3,7 +3,7 @@ const server = require("./src/server");
 
 //const { Character,Film,Planet}= require('../database/src/database/index');
 
-
+const PORT = 8004;
 /* Character.insert({
 _id:"101",
 name:"Jorge Vega",
@@ -12,6 +12,6 @@ gender: "male"
 }).then((res)=> console.log(res));
  */
 
-server.listen(8004,()=>{
-    console.log('Server Running in Port:'+ 8004 );
+server.listen(PORT,()=>{
+    console.log(`Server running in Port: ${PORT}` );
 });
